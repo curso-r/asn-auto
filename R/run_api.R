e23a76e5acb5719181e2ece7c9b083e69cd2e20c
Athos Damiani <athos.damiani@gmail.com>
2021-07-03 23:18:59 -0300

    sad

--- R/run_api.R
+++ R/run_api.R
@@ -2,4 +2,4 @@ library(plumber)
 
 p <- plumb("R/endpoints.R")
 
-p$run(host = "0.0.0.0", port = as.integer(Sys.getenv("PORT", unset = 8080)))
+p$run(port = as.integer(Sys.getenv("PORT", unset = 8080)), host = "0.0.0.0")
